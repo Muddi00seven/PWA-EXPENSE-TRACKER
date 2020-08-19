546462233540fba59213bfc36a2eef624f0ecd1e
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-08-19 20:13:25 +0500

    Balance Section Update

--- src/Components/TrackerComponents/Balance.tsx
+++ src/Components/TrackerComponents/Balance.tsx
@@ -1,9 +1,14 @@
 import React , { useContext} from 'react';
 import {TransactionContext} from '../../Context/GlobalContext';
+
 const Balance = () => {
+    const {Transaction} = useContext(TransactionContext);
+    const allAmount = Transaction.map((allAmount) => allAmount.amount)
+    const totalAmount = allAmount.reduce((a,b) => (a+=b) , 0).toFixed(2)
     return (
         <div>
-            
+            <h1>Your Balance</h1>
+            <h1 >${totalAmount}</h1>
         </div>
     )
 }
