42455d9cc74864f79ef7ffa3c67420f2ea3157fa
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-08-19 15:43:46 +0500

    update

--- src/Context/GlobalContext.tsx
+++ src/Context/GlobalContext.tsx
@@ -1,5 +1,5 @@
-import React, {useContext , useReducer} from 'react'
-import { createContext } from 'vm';
+import React, {createContext , useReducer} from 'react'
+// import { createContext ,useucer } from 'vm';
 import {TransactionType , initialStateType} from '../Types/TransactionType';
 import Reducer from './Reducer';
 
@@ -9,4 +9,40 @@ const initialState : initialStateType = {
     addTransaction : () => {}
 };
 
-export const TransactionContext = createContext(initialState)
+export const TransactionContext = createContext(initialState);
+
+export const TransactionProvider: React.FC = ({children}) =>{
+    const [state, dispatch] = useReducer(Reducer ,initialState);
+
+
+    function deleteTransaction(id: number) {
+        dispatch({
+            type: 'Delete_Transaction',
+            payload: id
+        })
+    }
+
+    function addTransaction(transactionData : TransactionType) {
+        dispatch({
+            type: 'Add_Transaction',
+            payload : transactionData
+        })
+    }
+
+    return(
+        <div>
+    
+         </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
